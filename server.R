52f92246fe9411440fd1806b3bd27d5425844a99
michada <michinho@gmail.com>
2014-06-25 22:14:58 +0200

    First functional version.

---
+++ server.R
@@ -0,0 +1,131 @@
+
+# This is the server logic for a Shiny web application.
+# You can find out more about building applications with Shiny here:
+# 
+# http://www.rstudio.com/shiny/
+#
+
+library(shiny)
+source('samplesize_MALDI.R')
+
+shinyServer(function(input, output, session) {
+  output$splitLocation <- renderUI({
+    sliderInput("split", "Split (Presence in Conditions)", 
+      value=min(input$split, input$numConditions-1),
+      min=1, 
+      max=input$numConditions-1, 
+      step=1, 
+      ticks=TRUE
+    );
+  })
+  
+  observe({ 
+    if (input$numConditions == 2) {
+      updateCheckboxGroupInput(session, "pattern", 
+        choices=list("1 vs 1" = "halfvshalf"),
+        selected="halfvshalf"
+      );
+    } else {
+      updateCheckboxGroupInput(session, "pattern",
+        choices=list(
+          "Half vs Half" = "halfvshalf",
+          "1 vs All" = "onevsall",
+          "1 vs 1 vs Noise" = "onevsone", 
+          "Split" = "split"
+        ),
+        selected=input$pattern
+      );
+    }
+  });
+  
+  sliderValue <- reactive({switch(input$pattern,
+    onevsall = function(index) {
+      ifelse (index == 1, input$presence, input$absence)
+    },
+    onevsone = function(index) {
+      ifelse (index == 1, input$presence,
+              ifelse (index == 2, input$absence, input$noise)
+      )
+    },
+    halfvshalf = function(index) {
+      ifelse(index %% 2 == 1, input$presence, input$absence)
+    },
+    split = function(index) {
+      ifelse(index <= input$split, input$presence, input$absence)
+    }
+  )});
+  
+  output$presenceByCondition <- renderUI({
+    input$applyPattern;
+    
+    lapply(1:input$numConditions, function(index) {
+      sliderInput(
+        inputId=paste("samples-", index, sep=""),
+        label=paste("Probability of Presence in Condition", index),
+        value=sliderValue()(index),
+        min=0,
+        max=1,
+        step=0.01,
+        format="0%"
+      )
+    })
+  });
+  
+  prob <- reactive({
+    sapply(1:input$numConditions, function(index) {
+      sampleId <- paste("samples-", index, sep="");
+      x <- input[[sampleId]];
+      
+      c(x/input$numConditions, (1-x)/input$numConditions);
+    });
+  });
+  
+  output$sampleSize <- renderText({
+    result <- computeSampleSizeMALDI(
+      prob(), 
+      input$numOfPeaks, 
+      alpha=input$alpha, 
+      power=input$power
+    );
+    paste("Num. of samples: ", ceiling(result$N), " (Exact: ", round(result$N, 2), ")", sep="");
+  });
+  
+  output$effectSize <- renderText({
+    result <- computeSampleSizeMALDI(prob(), input$numOfPeaks, alpha=input$alpha, power=input$power);
+    
+    cohenchies <- function(label) { cohen.ES(test="chisq", label)$effect.size };
+    label <- ifelse (result$w<cohenchies("small"), "small", ifelse(result$w<cohenchies("medium"), "medium", "big"))
+    
+    paste("Effect size: ", round(result$w, 4), " (", label, ")", sep="");
+  });
+  
+  output$methodDescription <- renderText({
+    result <- computeSampleSizeMALDI(prob(), input$numOfPeaks, alpha=input$alpha, power=input$power);
+    unadjustedAlpha <- input$alpha/input$numOfPeaks;
+    label <- effectSizeLabel(result$w);
+    
+    paste(
+      "<div style=\"text-align: justify\">",
+      "In MALDI-TOF-MS, we will consider the peak presence/absence in each ",
+      "sample (boolean values) as potential biomarkers. Chi-square test of ",
+      "independence will be used for each candidate biomarker to estimate its ",
+      "statistical significance (association between the presence and ",
+      "condition (", input$numConditions, " conditions tested)). In order to ",
+      "avoid false positives due to multiple tests, and considering ",
+      "approximately ", input$numOfPeaks, " biomarkers to be tested, for an ",
+      "adjusted p-value (with Bonferroni correction) of ", input$alpha, 
+      " (q-value), we will do the power calculation for a minimum ",
+      "significance threshold (alpha) of ", input$alpha, "/", input$numOfPeaks,
+      "=", unadjustedAlpha, " for every biomarker [1].<br/><br/>",
+      "In this sense, the power calculation for the association Chi-Square ",
+      "test with a power of ", round(input$power*100, 2), "%, alpha=", 
+      unadjustedAlpha, ", and a (", label, ") Chi Square effect size of ",
+      round(result$w, 4), ", <strong>the minimal number of samples is ", 
+      ceiling(result$N), "</strong>.<br/><br/>",
+      "[1] Witte JS, Elston RC, Cardon LR. On the relative sample size ",
+      "required for multiple comparisons. Stat Med. 2000 Feb 15;19(3):369-72. ",
+      "PubMed PMID: 10649302.", 
+      "</div>", sep=""
+    )
+  });
+})
---
+++ ui.R
@@ -0,0 +1,62 @@
+
+# This is the user-interface definition of a Shiny web application.
+# You can find out more about building applications with Shiny here:
+# 
+# http://www.rstudio.com/shiny/
+#
+
+library(shiny)
+
+#dataTable <- function(inputId, conditions = 2) {
+#  tagList(
+#    singleton(tags$head(tags$script(src = "js/dataTable.js")))
+#  )
+#}
+
+shinyUI(fluidPage(
+  # Application title
+  titlePanel("MALDI Sample Size Calculation (Presence/Absence)"),
+  
+  # Sidebar with a slider input for number of observations
+  sidebarLayout(position = "left",
+    sidebarPanel(
+      tags$h3("Parameters"),
+      numericInput("numConditions", "Num. of Conditions", value="2", min=2, max=100, step=1),
+      sliderInput("alpha", "Alpha", value=0.05, min=0.01, max=1, step=0.01),
+      sliderInput("power", "Power", value=0.8, min=0.5, max=0.95, step=0.01),
+      numericInput("numOfPeaks", "Num. of Peaks", value=2000, min=1, max=100000, step=1),
+      tags$hr(),
+      tags$h3("Presence Patterns"),
+      selectInput("pattern", "Pattern", 
+        choices=list("1 vs 1" = "halfvshalf")
+      ),
+      sliderInput("presence", "Presence means", value=0.8, min=0, max=1, step=0.01, format="0%"),
+      sliderInput("absence", "Absence means", value=0.2, min=0, max=1, step=0.01, format="0%"),
+      conditionalPanel(condition = "input.pattern == 'onevsone'",
+        sliderInput("noise", "Noise Means", value=0.5, min=0, max=1, step=0.01, format="0%")
+      ),
+      conditionalPanel(condition = "input.pattern == 'split'",
+        uiOutput("splitLocation")
+      ),
+      actionButton("applyPattern", "Apply Pattern")
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      fluidRow(
+        column(7, 
+            tags$h3("Peak Presence by Condition"),
+            uiOutput("presenceByCondition")
+        ),
+        column(5,
+          tags$h3("Samples Size"),
+          textOutput(outputId="sampleSize"),
+          textOutput(outputId="effectSize")      
+        )
+      ),
+      tags$hr(),
+      tags$h3("Method Description"),
+      htmlOutput(outputId="methodDescription")
+    )
+  )
+))
