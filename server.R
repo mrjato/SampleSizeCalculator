8e1ab526cb175fdce7d01c7a54e1701396e72bd8
michada <michinho@gmail.com>
2014-06-26 14:21:37 +0200

    Diagnostic test validation support.
    Diagnostic test validation allows the user to calculate the sample size needed for studies with two or more conditions.

--- server.R
+++ server.R
@@ -1,4 +1,3 @@
-
 # This is the server logic for a Shiny web application.
 # You can find out more about building applications with Shiny here:
 # 
@@ -22,18 +21,18 @@ shinyServer(function(input, output, session) {
   observe({ 
     if (input$numConditions == 2) {
       updateCheckboxGroupInput(session, "pattern", 
-        choices=list("1 vs 1" = "halfvshalf"),
-        selected="halfvshalf"
+        choices=list("1 vs 1" = "onevsone"),
+        selected="onevsone"
       );
     } else {
       updateCheckboxGroupInput(session, "pattern",
         choices=list(
           "Half vs Half" = "halfvshalf",
           "1 vs All" = "onevsall",
-          "1 vs 1 vs Noise" = "onevsone", 
+          "1 vs 1 vs Noise" = "onevsonevsnoise", 
           "Split" = "split"
         ),
-        selected=input$pattern
+        selected=ifelse(input$pattern=="onevsone", "halfvshalf", input$pattern)
       );
     }
   });
@@ -42,10 +41,13 @@ shinyServer(function(input, output, session) {
     onevsall = function(index) {
       ifelse (index == 1, input$presence, input$absence)
     },
-    onevsone = function(index) {
+    onevsonevsnoise = function(index) {
       ifelse (index == 1, input$presence,
-              ifelse (index == 2, input$absence, input$noise)
+        ifelse (index == 2, input$absence, input$noise)
       )
+    },
+    onevsone = function(index) {
+      ifelse(index %% 2 == 1, input$presence, input$absence)
     },
     halfvshalf = function(index) {
       ifelse(index %% 2 == 1, input$presence, input$absence)
@@ -80,7 +82,7 @@ shinyServer(function(input, output, session) {
     
     for (p in 1:nrow(m)) {
       rnames[p] <- paste("Peak", p);
-      presence <- 1;#ifelse(p <= 10, 1, 0);
+      presence <- ifelse(input$inversePattern, ifelse(p <= input$heatmapPeaks/2, 1, 0), 1);
       absence <- 1 - presence;
       
       for (c in 1:input$numConditions) {
@@ -118,7 +120,7 @@ shinyServer(function(input, output, session) {
       alpha=input$alpha, 
       power=input$power
     );
-    paste("Num. of samples: ", ceiling(result$N), " (Exact: ", round(result$N, 2), ")", sep="");
+    paste("<strong style=\"color: #11AA22;\">Number of samples: ", ceiling(result$N), "</strong>", sep="");
   });
   
   output$effectSize <- renderText({
@@ -127,7 +129,16 @@ shinyServer(function(input, output, session) {
     cohenchies <- function(label) { cohen.ES(test="chisq", label)$effect.size };
     label <- ifelse (result$w<cohenchies("small"), "small", ifelse(result$w<cohenchies("medium"), "medium", "big"))
     
-    paste("Effect size: ", round(result$w, 4), " (", label, ")", sep="");
+    paste("<strong style=\"color: #1122AA;\">Effect size: ", round(result$w, 4), " (", label, ")</strong>", sep="");
+  });
+  
+  output$heatmapSubtitle <- renderText({
+    result <- computeSampleSizeMALDI(prob(), input$numOfPeaks, alpha=input$alpha, power=input$power);
+    
+    cohenchies <- function(label) { cohen.ES(test="chisq", label)$effect.size };
+    label <- ifelse (result$w<cohenchies("small"), "small", ifelse(result$w<cohenchies("medium"), "medium", "big"))
+    
+    paste("<p style=\"color: grey;\">Peaks with <strong/>", round(result$w, 4), " (", label, ") effect size.</p>", sep="");
   });
   
   output$methodDescription <- renderText({
@@ -165,6 +176,71 @@ shinyServer(function(input, output, session) {
           "[1] Witte JS, Elston RC, Cardon LR. On the relative sample size 
           required for multiple comparisons. Stat Med. 2000 Feb 15;19(3):369-72.
           PubMed PMID: 10649302.", 
+        "</p>",
+      "</div>"
+    )
+  });
+  
+  output$tomSampleSize <- renderText({
+    acc <- input$tomAccuracy;
+    err <- input$tomError;
+    
+    zstar = qnorm(1 - (1 - input$tomCI) / 2);
+    samplesize <- zstar^2 * acc * (1-acc) / err^2;
+    
+    paste("<strong style=\"color: #11AA22;\">", ceiling(samplesize), "</strong>", sep="");
+  });
+  
+  output$tomMethodDescription <- renderText({
+    acc <- input$tomAccuracy*100;
+    ci <- input$tomCI*100;
+    err <- input$tomError*100;
+    ciWidth <- err*2;
+    
+    paste(sep="",
+      "<div style=\"text-align: justify;\">",
+        "<p>",
+          "We estimate the sample size for the minimum acceptable accuracy (", 
+          acc, "%) and a width of the ", ci, "% confidence interval of ", 
+          ciWidth, "% (", max(0, acc - err), "%, ", min(100, acc + err), "%).",
+        "</p>",
+      "</div>"
+    )
+  });
+  
+  output$twoSampleSize <- renderText({
+    sens <- input$twoSensitivity;
+    spec <- input$twoSpecificity;
+    err <- input$twoError;
+    
+    zstar = qnorm(1 - (1 - input$twoCI) / 2);
+    sensSampleSize <- ceiling(zstar^2 * sens * (1-sens) / err^2);
+    specSampleSize <- ceiling(zstar^2 * spec * (1-spec) / err^2);
+    sampleSize <- sensSampleSize + specSampleSize;
+    
+    paste(sep="",
+      "<strong style=\"color: #11AA22;\">", 
+        sampleSize, 
+        " (", sensSampleSize, " diseased, ", specSampleSize, " healthy)",
+      "</strong>"
+    );
+  });
+  
+  output$twoMethodDescription <- renderText({
+    sens <- input$twoSensitivity*100;
+    spec <- input$twoSpecificity*100;
+    ci <- input$twoCI*100;
+    err <- input$twoError*100;
+    ciWidth <- err*2;
+    
+    paste(sep="",
+      "<div style=\"text-align: justify;\">",
+        "<p>",
+          "We estimate the sample size for the minimum acceptable sensitivity (", 
+          sens, "%) and specificity (", spec, "%), and a width of the ", ci, 
+          "% confidence interval of ", ciWidth, "%.",
+          "Sensitivity CI: (", max(0, sens - err), "%, ", min(100, sens + err), "%). ",
+          "Specificity CI: (", max(0, spec - err), "%, ", min(100, spec + err), "%). ",
         "</p>",
       "</div>"
     )
--- ui.R
+++ ui.R
@@ -1,4 +1,3 @@
-
 # This is the user-interface definition of a Shiny web application.
 # You can find out more about building applications with Shiny here:
 # 
@@ -7,64 +6,146 @@
 
 library(shiny)
 
-#dataTable <- function(inputId, conditions = 2) {
-#  tagList(
-#    singleton(tags$head(tags$script(src = "js/dataTable.js")))
-#  )
-#}
-
 shinyUI(fluidPage(
   # Application title
   titlePanel("MALDI Sample Size Calculation (Presence/Absence)"),
   
   # Sidebar with a slider input for number of observations
-  sidebarLayout(position = "left", fluid=TRUE,
-    sidebarPanel(
-      tags$h3("Parameters"),
-      fluidRow(
-        column(4, numericInput("numConditions", "Conditions", value="2", min=2, max=100, step=1)),
-        column(7, offset=1,numericInput("numOfPeaks", "Peaks in Experiment", value=2000, min=1, max=100000, step=1))
-      ),
-      sliderInput("alpha", "Alpha", value=0.05, min=0.01, max=1, step=0.01),
-      sliderInput("power", "Power", value=0.8, min=0.5, max=0.95, step=0.01),
-      
-      tags$hr(),
-      tags$h3("Presence Patterns"),
-      selectInput("pattern", "Pattern", 
-        choices=list("1 vs 1" = "halfvshalf")
-      ),
-      sliderInput("presence", "Presence means", value=0.8, min=0, max=1, step=0.01, format="0%"),
-      sliderInput("absence", "Absence means", value=0.2, min=0, max=1, step=0.01, format="0%"),
-      conditionalPanel(condition = "input.pattern == 'onevsone'",
-        sliderInput("noise", "Noise Means", value=0.5, min=0, max=1, step=0.01, format="0%")
-      ),
-      conditionalPanel(condition = "input.pattern == 'split'",
-        uiOutput("splitLocation")
-      ),
-      actionButton("applyPattern", "Apply Pattern")
-    ),
-    
-    # Show a plot of the generated distribution
-    mainPanel(
-      fluidRow(
-        column(7, 
-            tags$h3("Peak Presence"),
-            uiOutput("presenceByCondition"),
-            tags$hr(),
-            tags$h3("Sample Heatmap"),
-            fluidRow(
-              column(6, numericInput("heatmapSamples", label="Samples by Condition", min=1, max=50, value=10, step=1)),
-              column(6, numericInput("heatmapPeaks", label="Peaks", min=1, max=100, value=20, step=1))
-            ),
-            plotOutput(outputId="heatmap")
-        ),
-        column(5, 
-          tags$h3("Sample Size"),
-          textOutput(outputId="sampleSize"),
-          textOutput(outputId="effectSize"),
+  tabsetPanel(
+    tabPanel("Biomarker Discovery", 
+      sidebarLayout(position = "left", fluid=TRUE,
+        sidebarPanel(
+          tags$h3("Parameters"),
+          fluidRow(
+            column(4, numericInput("numConditions", "Conditions", value=4, min=2, max=100, step=1)),
+            column(7, offset=1,numericInput("numOfPeaks", "Peaks in Experiment", value=2000, min=1, max=100000, step=1))
+          ),
+          sliderInput("alpha", "Alpha", value=0.05, min=0.01, max=1, step=0.01),
+          tags$p(style="font-size: 0.8em; color: gray;", 
+            "Probability of detecting a peak as a potential biomarker when it is not
+            a real biomarker (Type I error prob.)."
+          ),
+          sliderInput("power", "Power", value=0.8, min=0.5, max=0.95, step=0.01),
+          tags$p(style="font-size: 0.8em; color: gray;", 
+            "Probability of detecting a peak as a potential biomarker when it is is
+            a real biomarker (1-beta is the Type II error prob.)."
+          ),
+          
           tags$hr(),
-          tags$h3("Method Description"),
-          htmlOutput(outputId="methodDescription")
+          tags$h3("Presence Patterns"),
+          selectInput("pattern", "Pattern", 
+            choices=list("1 vs 1" = "halfvshalf")
+          ),
+          sliderInput("presence", "Presence means", value=0.8, min=0, max=1, step=0.01, format="0%"),
+          sliderInput("absence", "Absence means", value=0.2, min=0, max=1, step=0.01, format="0%"),
+          conditionalPanel(condition = "input.pattern == 'onevsone'",
+            sliderInput("noise", "Noise Means", value=0.5, min=0, max=1, step=0.01, format="0%")
+          ),
+          conditionalPanel(condition = "input.pattern == 'split'",
+            uiOutput("splitLocation")
+          ),
+          actionButton("applyPattern", "Apply Pattern")
+        ),
+        
+        # Show a plot of the generated distribution
+        mainPanel(
+          fluidRow(
+            column(7, 
+              tags$h3("Presence Pattern"),
+              tags$p(style="font-size: 0.9em; color: gray;", 
+                "This presence pattern allows you to easily establish your effect
+                size."
+              ),
+              uiOutput("presenceByCondition"),
+              htmlOutput("effectSize"),
+              tags$hr(),
+              tags$h3("Sample Heatmap (Simulation)"),
+              htmlOutput("heatmapSubtitle"),
+              fluidRow(
+                column(6, numericInput("heatmapSamples", label="Samples by Condition", min=1, max=50, value=10, step=1)),
+                column(6, numericInput("heatmapPeaks", label="Peaks", min=1, max=100, value=20, step=1))
+              ),
+              checkboxInput("inversePattern", label="Show also inverse pattern", value=FALSE),
+              plotOutput(outputId="heatmap")
+            ),
+            column(5,
+              tags$div(class="well",
+                tags$h3("Sample Size"),
+                htmlOutput(outputId="sampleSize")
+              ),
+              tags$hr(),
+              tags$h3("Method Description"),
+              htmlOutput(outputId="methodDescription")
+            )
+          )
+        )
+      )
+    ),
+    tabPanel("Diagnostic Test Validation", 
+      tabsetPanel(
+        tabPanel("Two Conditions",
+         sidebarLayout(position = "left", fluid=TRUE,
+           sidebarPanel(
+              tags$h3("Parameters"),
+              sliderInput("twoSensitivity", label="Sensitivity", min=0, max=0.99, step=0.01, value=0.8, format="0%"),
+              tags$p(style="font-size: 0.8em; color: gray;", 
+                "The minimum acceptable sensitivity for your diagnostic test (be conservative).
+                Sensitivity is the probability of a diseased sample being diagnosted as such."
+              ),
+              sliderInput("twoSpecificity", label="Specificity", min=0, max=0.99, step=0.01, value=0.8, format="0%"),
+              tags$p(style="font-size: 0.8em; color: gray;", 
+                "The minimum acceptable specificity for your diagnostic test (be conservative).
+                Specificity is the probability of a healthy sample being diagnosted as such."
+              ),
+              sliderInput("twoError", label="Error", min=0, max=0.5, step=0.01, value=0.05, format="0%"),
+              tags$p(style="font-size: 0.8em; color: gray;", 
+                "The maximum acceptable error for your diagnostic test (be conservative).
+                It determines the width of the confidence interval. Typically 5%."
+              ),
+              sliderInput("twoCI", label="Confidence Inteval", min=0.8, max=0.99, step=0.01, value=0.95, format="0%"),
+              tags$p(style="font-size: 0.8em; color: gray;", 
+                "Probability of the accuracy fall in (accuracy-error, accuracy+error) interval.
+                Typically 90%, 95% or 99%."
+              )
+           ),
+           mainPanel(
+             tags$div(class="well well-lg",
+               tags$h3("Sample Size"),
+               htmlOutput("twoSampleSize")
+             ),
+             tags$h3("Method Description"),
+             htmlOutput("twoMethodDescription")
+           )
+         )
+        ),
+        tabPanel("Two or More Conditions",
+          sidebarLayout(position = "left", fluid=TRUE,
+            sidebarPanel(
+              tags$h3("Parameters"),
+              sliderInput("tomAccuracy", label="Accuracy", min=0, max=0.99, step=0.01, value=0.8, format="0%"),
+              tags$p(style="font-size: 0.8em; color: gray;", 
+                "The minimum acceptable accuracy for your diagnostic test (be conservative)."
+              ),
+              sliderInput("tomError", label="Error", min=0, max=0.5, step=0.01, value=0.05, format="0%"),
+              tags$p(style="font-size: 0.8em; color: gray;", 
+                "The maximum acceptable error for your diagnostic test (be conservative).
+                It determines the width of the confidence interval. Typically 5%."
+              ),
+              sliderInput("tomCI", label="Confidence Inteval", min=0.8, max=0.99, step=0.01, value=0.95, format="0%"),
+              tags$p(style="font-size: 0.8em; color: gray;", 
+                "Probability of the accuracy fall in (accuracy-error, accuracy+error) interval.
+                Typically 90%, 95% or 99%."
+              )
+            ),
+            mainPanel(
+              tags$div(class="well well-lg",
+                tags$h3("Sample Size"),
+                htmlOutput("tomSampleSize")
+              ),
+              tags$h3("Method Description"),
+              htmlOutput("tomMethodDescription")
+            )
+          )
         )
       )
     )
